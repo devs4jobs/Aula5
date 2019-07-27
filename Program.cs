b31a4200e71c903e166e33046f628726b791c4d4
Dev4Jobs <devjobs@gmail.com>
2019-07-27 11:00:23 -0300

    Start

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aula5
+{
+    class Program
+    {
+        public static List<BuscaCep> cepsBuscados = new List<BuscaCep>();
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Informe o cep");
+                var cep = Console.ReadLine();
+
+                if (cep.Length < 8)
+                    Console.WriteLine("CEP inválido!");
+
+                cepsBuscados.Add(httprequest.GetUrlAsync($"https://viacep.com.br/ws/{cep}/json/").Result);
+                Console.WriteLine($"Total de ceps consultados/cadastrados: {cepsBuscados.Count}");
+
+                Console.WriteLine("Digite 1 para exibir o primeiro registro \nDigite 2 para exibir o último registro");
+                var opcao = Console.ReadLine();
+
+                switch (opcao)
+                {
+                    case "1": { Console.WriteLine(JsonConvert.SerializeObject(cepsBuscados.First())); break; }
+                    case "2": { Console.WriteLine(JsonConvert.SerializeObject(cepsBuscados.Last())); break; }
+                    default:
+                        {
+                            Console.WriteLine("Opção inválida!");
+                            break;
+                        }
+
+                }
+            }
+
+        }
+    }
+}
---
+++ httprequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Aula5
+{
+    public static class httprequest
+    {
+        public static async Task<BuscaCep> GetUrlAsync(string url)
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync(url);
+
+            if ((int)response.StatusCode != 200)
+                Console.WriteLine("CEP inválido");
+
+            var resultado = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(resultado);
+
+            return JsonConvert.DeserializeObject<BuscaCep>(resultado);
+
+        }
+    }
+}
